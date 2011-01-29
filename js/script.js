44de4c58e8846f8dacf650421601eb3f67574d59
xonecas <seancaetanomartin@gmail.com>
2011-01-29 07:34:21 +0000

    made some improvements, now using images for aliens, and cannon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 /* Author: xonecas
 
+   this file needs to be cleaned up and organized
+
 */
 
 (function (window, undefined) {
@@ -9,15 +11,14 @@
    $(window).load(function () {
       $('#fontLoader').remove();
 
-      if (Modernizr.canvas && Modernizr.canvastext) {
+      if (Modernizr.canvas) {
 
          var canvas = document.createElement('canvas'),
             canvasWidth = $(window).width() - 38,
             canvasHeight = $(window).height(),
-            startX = Math.ceil(canvasWidth / 2),
-            startY = canvasHeight - 20,
-            currentX = startX,
-            currentY = startY;
+            center = Math.ceil(canvasWidth / 2),
+            currentX = center + 10,
+            currentY = canvasHeight - 58;
 
 
          $(canvas).attr({
@@ -28,31 +29,60 @@
          $('#main').html(canvas);
 
          var ctx = canvas.getContext('2d');
-         ctx.font = '38px oxy';
-         ctx.fillStyle = '#444444';
+         var alien = $("#alien").get(0),
+            laser = $('#laser').get(0);
 
-         ctx.fillText('o', startX, startY);
+         ctx.fillStyle = '#fff';
+
+         ctx.drawImage(laser, currentX, currentY, 38, 38);
+
+         var z = 11, startx = center - 279, consty = 20;
+         while (z--) {
+            ctx.drawImage(alien, startx, consty, 38, 38);
+            startx += 58;
+         }
+
 
          $(document).keydown(function (ev) {
             switch(ev.which) {
-               case 37:
-               case 39:
+               case 37: // left arrow
+               case 39: // right arrow
                   var keepMoving = 10;
                   (function animate () {
                      if (currentX <= 38 ||
                         currentX >= (canvasWidth -38))
-                     return currentX = (currentX <= 38)? 40: canvasWidth -40;
+                        return currentX = (currentX <= 38)? 40: canvasWidth -40;
 
-                     ctx.fillStyle = '#ffffff';
-                     ctx.fillRect(38, 38, currentX, currentY);
+                     ctx.fillStyle = '#444';
+                     ctx.fillRect(currentX -2, currentY , 42, 38);
 
                      currentX += (ev.which === 37)? -2: 2;
 
-                     ctx.fillStyle = '#444444';
-                     ctx.fillText('o', currentX, currentY);
+                     ctx.fillStyle = '#ffffff';
+                     ctx.drawImage(laser, currentX, currentY, 38, 38);
                      if (--keepMoving)
                         setTimeout(animate, 20);
-                  })();
+                  }) ();
+               break;
+               
+               case 32: // space bar
+                  var originX = currentX +18,
+                     originY = currentY;
+
+                  (function fly () {
+                     ctx.fillStyle = '#444';
+                     ctx.fillRect(originX, originY, 2, 10);
+
+                     originY -= 22;
+                     ctx.fillStyle = '#fff';
+                     ctx.fillRect(originX, originY, 2, 10);
+                    
+                     if (originY >= -10)
+                        setTimeout(fly, 30);
+                  }) ();
+               break;
+               default:
+                  ev.preventDefault();
             }
 
             //log(ev.which);
